cca116e0f6b1d055fce14c2623c7e55f87a4b4a2
cod3xW <59795724+cod3xW@users.noreply.github.com>
2026-04-27 19:41:40 +0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,14 +1,16 @@
 export default async function handler(req, res) {
-  const { team, season } = req.query;
+  const { team, search, action } = req.query;
 
-  const response = await fetch(
-    `https://v3.football.api-sports.io/fixtures?team=${team}&last=10`,
-    {
-      headers: {
-        'x-apisports-key': process.env.API_KEY
-      }
-    }
-  );
+  let url;
+  if (action === 'search') {
+    url = `https://v3.football.api-sports.io/teams?search=${search}`;
+  } else {
+    url = `https://v3.football.api-sports.io/fixtures?team=${team}&last=10`;
+  }
+
+  const response = await fetch(url, {
+    headers: { 'x-apisports-key': process.env.API_KEY }
+  });
 
   const data = await response.json();
   res.status(200).json(data);
