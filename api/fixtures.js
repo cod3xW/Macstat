37c0e47db722abfcfa1ceb80b4093499908d6199
cod3xW <59795724+cod3xW@users.noreply.github.com>
2026-04-28 00:33:53 +0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   } else if (action === 'today') {
     const today = new Date().toISOString().split('T')[0];
     url = `https://v3.football.api-sports.io/fixtures?date=${today}`;
-  }} else if (action === 'standings') {
+  } else if (action === 'standings') {
     url = `https://v3.football.api-sports.io/standings?league=${league}&season=2025`;
   } else if (action === 'upcoming') {
     url = `https://v3.football.api-sports.io/fixtures?league=${league}&next=10`;
